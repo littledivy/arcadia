a36bae55218e8a69052e95005a9b7dca208bd0ed
Divy Srivastava <dj.srivastava23@gmail,com>
2020-03-30 11:35:12 +0530

    add mocha tests :coffee:

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ const config = {};
 
 config.port = process.env.PORT || 5000;
 config.dbURL = process.env.DATABASEURL || "mongodb://localhost/arcadia";
-
+config.testDbURL = process.env.TESTDATABASEURL || "mongodb://localhost/arcadia-test";
 module.exports = config;
